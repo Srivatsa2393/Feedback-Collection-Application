cea892bcfb4cede35972a16f3b4266ec60d07d5a
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-09 18:02:05 +0530

    Heroku deployment checklist

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const app = express();
 
 //create a route handler
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+  res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+//Dynamically figure out what port to listen to
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
