854ac7aefbbf6b683e4a0da93aa5b1a8f6e4f48e
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-10 02:46:34 +0530

    Passport Setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //create our first express app
 const app = express();
 
-//create a route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'Srivatsa youre doing a fullstack project' });
-});
+passport.use(new GoogleStrategy());
 
 //Dynamically figure out what port to listen to
 const PORT = process.env.PORT || 5000;
