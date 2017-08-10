3ba42c475d958784554c850470d6326f83da16c7
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-10 13:27:03 +0530

    Google strategy options

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 //create our first express app
 const app = express();
 
-passport.use(new GoogleStrategy());
+//console.developers.google.com
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 //Dynamically figure out what port to listen to
 const PORT = process.env.PORT || 5000;
