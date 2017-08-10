64ee68e938342b0aef465662891d95140f0a7628
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-10 18:53:55 +0530

    Connecting Mongoose to Mongo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+//requiring mongoose
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 //we are not assigning anything so we can just do require
 require('./services/passport');
+
+//instruct mongoose to connect to mongo db
+mongoose.connect(keys.mongoURI);
 
 //create our first express app
 const app = express();
