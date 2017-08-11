41b2c039d5ddc1988e0dacea6f4812931d943fe3
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-11 16:03:59 +0530

    Mongoose Queries

--- server/services/passport.js
+++ server/services/passport.js
@@ -16,11 +16,22 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have record with the given profile id
+          done(null, existingUser);
+        } else {
+          //we dont have a user record with this id, make a new record
+          //this creates a new model instance and then save the instance and then we cretae a second instance
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
       // console.log('access token', accessToken);
       // console.log('refresh token', refreshToken);
       // console.log('profile:', profile);
       //to create a new instance of the User
-      new User({ googleId: profile.id }).save();
     }
   )
 );
