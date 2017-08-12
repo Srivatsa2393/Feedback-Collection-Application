55fa0a67dbbb6b61a7ed1a4678a84c7d2a07e7b3
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-12 22:35:04 +0530

    Refactoring the GoogleStrategy functionality with the async await

--- server/services/passport.js
+++ server/services/passport.js
@@ -25,25 +25,38 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //we already have record with the given profile id
-          done(null, existingUser);
-        } else {
-          //we dont have a user record with this id, make a new record
-          //this creates a new model instance and then save the instance and then we cretae a second instance and we used promises inside the callback
-          //to create a new instance of the User
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
-      // console.log('access token', accessToken);
-      // console.log('refresh token', refreshToken);
-      // console.log('profile:', profile);
+    // (accessToken, refreshToken, profile, done) => {
+    //   User.findOne({ googleId: profile.id }).then(existingUser => {
+    //     if (existingUser) {
+    //       //we already have record with the given profile id
+    //       done(null, existingUser);
+    //     } else {
+    //       //we dont have a user record with this id, make a new record
+    //       //this creates a new model instance and then save the instance and then we cretae a second instance and we used promises inside the callback
+    //       //to create a new instance of the User
+    //       new User({ googleId: profile.id })
+    //         .save()
+    //         .then(user => done(null, user));
+    //     }
+    //   });
+    //   // console.log('access token', accessToken);
+    //   // console.log('refresh token', refreshToken);
+    //   // console.log('profile:', profile);
+    // }
+
+    //refactoring with async await
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        //we already have record with the given profile id
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
