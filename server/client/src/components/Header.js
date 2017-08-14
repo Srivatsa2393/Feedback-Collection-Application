52d01567e4249c49a683bbf6f6a605cbf2d00a34
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-14 16:11:50 +0530

    Header Content

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,11 +5,19 @@ class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return 'Still deciding';
+        return;
       case false:
-        return 'i am logged out';
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
       default:
-        return 'i am logged in';
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
     }
   }
 
