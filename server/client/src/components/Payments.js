4a390cf71671a6a83bb3bbb7ad8c4c1709c7b42c
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-14 20:54:35 +0530

    Stripe tokens

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
-class Payments extends Component{
+class Payments extends Component {
   render() {
-    debugger();
-
-    return(
+    return (
       <StripeCheckout
         amount={500}
         token={token => console.log(token)}
