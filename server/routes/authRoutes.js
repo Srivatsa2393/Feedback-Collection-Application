552ba42adde72a7cf1e96f89e590ce9fcbdf806e
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-15 16:27:08 +0530

    Adding a prompt to select a particular account each time on login

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,7 +6,8 @@ module.exports = app => {
   app.get(
     '/auth/google',
     passport.authenticate('google', {
-      scope: ['profile', 'email']
+      scope: ['profile', 'email'],
+      prompt: 'select_account'
     })
   );
 
