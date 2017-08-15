39ca26005402397e1744a1a16bb214baffc015d7
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-16 00:48:11 +0530

    creating another middleware to verify the minimum credits

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,7 +1,8 @@
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     //we want to make sure user is logged in
     //if you are logged in we must make sure that you have enough credits to send a survey
   });
