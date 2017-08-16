9c3c2c4d4b2045d5ff4373e06553ea72fd8094ab
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-16 21:53:06 +0530

    Improving the email template

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+//Development testing only axios helpers
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
@@ -16,3 +20,6 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 );
+
+// console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+// console.log('Environment is', process.env.NODE_ENV);
