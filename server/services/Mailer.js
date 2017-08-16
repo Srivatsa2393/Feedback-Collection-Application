4207e050f7e4fb94b388d21af0bfa54a98c0add4
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-16 17:48:58 +0530

    Mailer setup

---
+++ server/services/Mailer.js
@@ -0,0 +1,7 @@
+const sendgrid = require('sendgrid');
+//helper object that creates a mailer
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+//setup a class
+class Mailer extends helper.Mail {}
