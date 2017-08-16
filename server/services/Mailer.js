b7183f878edb2b8cd15067c979204bd5661db2b1
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-16 19:12:54 +0530

    Mailer constructor

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -4,6 +4,17 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 //setup a class
-class Mailer extends helper.Mail {}
+//content is the html string obtained from surveyTemplate(survey) and the initial destructuring is the survey object
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    //sendgrid stuff
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
 
 module.exports = Mailer;
