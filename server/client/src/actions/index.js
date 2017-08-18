7944a549fb21b4e6954baa168f1cee0e75c2eadd
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-18 19:56:54 +0530

    Posting to surveys

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -23,8 +23,8 @@ export const handleToken = token => async dispatch => {
 };
 
 //submitting a survey action creator
-export const submitSurvey = values => {
-  return {
-    type: 'submit_survey'
-  };
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
